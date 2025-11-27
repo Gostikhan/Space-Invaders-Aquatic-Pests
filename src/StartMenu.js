deb7f39525b9b96f9e67c4d4fbbeb821d5fabe0e
Georgy Stikhanovskiy <gst87@uclive.ac.nz>
2025-11-27 15:57:10 +1300

    Quick changes

--- src/StartMenu.js
+++ src/StartMenu.js
@@ -4,14 +4,13 @@ SpaceInvaders.StartMenu = function(game) {
 }
 
 SpaceInvaders.StartMenu.prototype = {
-	
 	create: function () {
 		startBG = this.add.image(0, 0, 'titlescreen');
 		startBG.inputEnabled = true;
 		startBG.events.onInputDown.addOnce(this.startGame, this);		
-		startPrompt = this.add.bitmapText(this.world.centerX-180, this.world.centerY, 'eightbitwonder', "Protect the lake!", 24);
+		startPrompt = this.add.bitmapText(this.world.centerX, this.world.centerY, 'eightbitwonder', "Protect the lake!", 24);
+		
 	},
-
 	startGame: function (pointer) {
 		this.state.start('Game');
 	}
